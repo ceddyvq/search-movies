f6a614482c9cc7157ae9c5434278ae979413146a
ceddyvq <ceddyvq@gmail.com>
2021-03-24 00:51:38 -0500

    modifique app.js y searchForm.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import './App.css';
 import 'bulma/css/bulma.css'
 
 function App() {
+  state ={results:[]}
+
+  _handleResults=(results)=> {
+    this.setState({results})
+  }
+  _renderResults() {
+    const {results}=this.state
+    return results.map(movie=>{
+      return <p key={movie.imdbID}>{movie.Title} </p>
+    })
+  }
+
+
   return (
     <div className="App">
       <Title>Search movies</Title>
       <div className='SearchForm-wrapper'>
-        <SearchForm/>
+        <SearchForm onResults= {this_handleResults} />
       </div>
+      {this.state.results.length === 0 
+        ?<p>Sin resultados </p>
+        : this._renderResults()
+      }
     </div>
   );
 }
