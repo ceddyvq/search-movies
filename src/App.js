cf03186dfba3348acec376b5358fbe756c24c024
ceddyvq <ceddyvq@gmail.com>
2021-03-23 22:49:17 -0500

    modificacion de search form y primera subida  a github

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Title} from './components/Title'
+import {SearchForm} from './components/SearchForm'
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Title>Search movies</Title>
+      <div className='SearchForm-wrapper'>
+        <SearchForm/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
